375ddfe98ba53559128c2a8271ecb38fe5743284
Fernando Paladini <fnpaladini@gmail.com>
2020-11-06 21:32:38 -0300

    Add product migration and make it available to access PostgreSQL Docker instance externally

--- favorites_list/models.py
+++ favorites_list/models.py
@@ -11,3 +11,18 @@ class Client(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Product(models.Model):
+
+    class Meta:
+
+        db_table = 'products'
+
+    price = models.CharField(max_length=50)
+    image = models.CharField(max_length=2048) # max chars that an URL can have
+    brand = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    reviewScore = models.DecimalField(max_length=200, decimal_places=2, max_digits=2)
+
+    def __str__(self):
+        return self.title
